ccdbb7e52136715535d521e800e98397974f4b4d
Guillaume Bagard <guillaume.bgrd@icloud.com>
2025-05-01 13:03:28 +0200

    correction conflits avec les branches

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Charger le fichier FXML principal (AdminController.fxml)
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/admin.fxml"));
-        AnchorPane root = loader.load();
-
-        // Initialiser la scène
-        Scene scene = new Scene(root);
-
-        // Définir la scène et le titre
-        primaryStage.setTitle("Panneau d'Administration");
-        primaryStage.setScene(scene);
-
-        // Afficher la fenêtre
+        System.out.println(">> FXML lookup: " + getClass().getResource("/view/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setTitle("Boutique");
         primaryStage.show();
     }
-
-    public static void main(String[] args) {
-        launch(args); // Lance l'application
-    }
+    public static void main(String[] args) { launch(args); }
 }
---
+++ src/MainAdmin.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+public class MainAdmin extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Charger le fichier FXML principal (AdminController.fxml)
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/admin.fxml"));
+        AnchorPane root = loader.load();
+
+        // Initialiser la scène
+        Scene scene = new Scene(root);
+
+        // Définir la scène et le titre
+        primaryStage.setTitle("Panneau d'Administration");
+        primaryStage.setScene(scene);
+
+        // Afficher la fenêtre
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Lance l'application
+    }
+}
