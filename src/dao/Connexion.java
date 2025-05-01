956ab839d38e89b92a3d9c52ff11a574b6307c91
Guillaume Bagard <guillaume.bgrd@icloud.com>
2025-05-02 01:54:45 +0200

    maj

--- src/dao/Connexion.java
+++ src/dao/Connexion.java
@@ -5,11 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connexion {
-<<<<<<< HEAD
-    private static final String URL = "jdbc:mysql://localhost:1406/shopping_db";
-=======
+
     private static final String URL = "jdbc:mysql://localhost:8889/shopping_db";
->>>>>>> c897d6b (commencement controller page InventaireAdmin)
     private static final String USER = "root";
     private static final String PASSWORD = "root"; // adapte si besoin
 
