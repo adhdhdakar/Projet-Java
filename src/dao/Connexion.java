42265d8cdb38db04dbc0a105144d28776bb25bbc
hugo-frr <hugo.ferre@edu.ece.fr>
2025-05-10 12:21:24 +0200

    possibilité JPEG et PNG

--- src/dao/Connexion.java
+++ src/dao/Connexion.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class Connexion {
 
-    private static final String URL = "jdbc:mysql://localhost:8889/shopping_db";
+    private static final String URL = "jdbc:mysql://localhost:1406/shopping_db";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
